4451c46d675d3fc3686a93f1cf46b24c14923c22
sanuzzi <occhiuzzi.santiago@gmail.com>
2017-04-25 11:33:08 -0300

    Creamos las bebidas
    Están las bebidas grogXD, jarraLoca y klusener

--- clientes.hs
+++ clientes.hs
@@ -10,3 +10,37 @@ type Resistencia = Double
 
 rodri = Cliente "Rodrigo" 150 []
 marcos = Cliente "Marcos" 40 [rodri]
+
+bajarResistencia cuánto cliente =
+  nuevaResistencia (resistencia cliente - cuánto) cliente
+
+afectarAmigos criterio cliente =
+  cliente {
+    amigos = (map criterio . amigos) cliente
+  }
+
+jarraLoca =
+  bajarResistencia 10 .
+  afectarAmigos (bajarResistencia 10)
+
+nuevaResistencia unaResistencia cliente =
+  cliente {
+    resistencia = unaResistencia
+  }
+
+grogXD = nuevaResistencia 0
+
+klusener nombre =
+  bajarResistencia ((fromIntegral . length) nombre)
+
+
+
+
+
+
+
+
+
+
+
+--
